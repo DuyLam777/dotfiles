2951219f08575d7dc8ebb09e7e3b6469036f6cf3
DuyLam777 <ngocduy.lam@student.kdg.be>
2024-10-12 02:06:39 +0200

    change the fonts to zed plex mono

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,8 +8,8 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = 'Kanagawa (Gogh)'
-config.font = wezterm.font 'Hack Nerd Font'
-config.font_size = 10.0
+config.font = wezterm.font('Zed Plex Mono', { weight = 400})
+config.font_size = 12.0
 config.window_background_opacity = 1
 -- Tab bar
 config.enable_tab_bar = false
